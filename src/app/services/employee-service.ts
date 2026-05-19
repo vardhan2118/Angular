5eabe4e8614bc5cc0615498003e0b510f0004f8f
Chitravardhan Reddy <cchitravardhan21@gmail.com>
2026-05-19 15:41:38 +0530

    Added Back-end url

--- src/app/services/employee-service.ts
+++ src/app/services/employee-service.ts
@@ -3,13 +3,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EmployeeModel } from '../models/employee-model';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
+  private jsonUrl = 'data/employees.json';
 
-   private jsonUrl = 'data/employees.json';
+  // private jsonUrl = 'http://localhost:8080/getAllEmployees'
 
   constructor(private http: HttpClient) {}
 
